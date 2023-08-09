c03349ba14c5241651f37f01effea57d94328e0e
Matias Teti <matiasteti.alq@gmail.com>
2023-08-09 00:09:47 -0300

    subiendo repo

--- app1/views.py
+++ app1/views.py
@@ -3,6 +3,7 @@ from django.template import Template, Context
 from django.http import HttpResponse
 from .models import Curso
 
+
 # Create your views here.
 
 def probandoTemplate(self):
@@ -20,14 +21,13 @@ def probandoTemplate(self):
 
 def curso_list(request):
     curso = Curso.objects.all()
-    return render(request, 'curso_list.html', {'curso':curso})
+    return render(request, 'curso_list.html', {'curso': curso})
 
 def curso_create(request):
-    if request.method == 'POST':
-        nombre = request.POST.get('nombre')
-        numero_comision = request.POST.get('numero_comision')
+    if request.method == 'post':
+        nombre = request.post.get('nombre')
+        numero_comision = request.post.get('numero_comision')
         curso = Curso(nombre=nombre, numero_comision=numero_comision)
         curso.save()
-        return redirect('curso_list')
-    
+        return redirect('curso_create')
     return render(request, 'curso_create.html')
