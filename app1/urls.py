5c9d246ada5635f564f22326ceff8b8c5d62211c
Matias Teti <matiasteti.alq@gmail.com>
2023-08-11 15:35:17 -0300

    Mensaje commit

--- app1/urls.py
+++ app1/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('probandoTemplate/', views.probandoTemplate, name='probandoTemplate'),
     path('index/', views.index, name='index'),
+    path('curso_list/', views.curso_list, name='curso_list'),
+    path('curso_create/', views.curso_create, name='curso_create'),
 ]
--- app1/views.py
+++ app1/views.py
@@ -7,8 +7,6 @@ import datetime
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'index.html')
 
 def probandoTemplate(request):
    
@@ -30,3 +28,18 @@ def probandoTemplate(request):
 
     return HttpResponse(documento)
 
+def index(request):
+    return render(request, 'index.html')
+
+def curso_list(request):
+    curso = Curso.objects.all()
+    return render(request, 'curso_list.html', {'curso' : curso})
+
+def curso_create(request):
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        numero_comision = request.POST.get('numero_comision')
+        curso = Curso(nombre=nombre, numero_comision=numero_comision)
+        curso.save()
+        return redirect('curso_list')
+    return render(request, 'curso_create.html')
