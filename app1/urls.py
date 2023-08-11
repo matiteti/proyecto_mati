5ffd1c945d401be65615ffea6f9fd2bb212d8b4a
Matias Teti <matiasteti.alq@gmail.com>
2023-08-11 16:20:24 -0300

    Mensaje commit

--- app1/urls.py
+++ app1/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('probandoTemplate/', views.probandoTemplate, name='probandoTemplate'),
     path('index/', views.index, name='index'),
-    path('curso_list/', views.curso_list, name='curso_list'),
     path('curso_create/', views.curso_create, name='curso_create'),
+    path('curso_list/', views.curso_list, name='curso_list'),
 ]
--- app1/views.py
+++ app1/views.py
@@ -8,26 +8,6 @@ import datetime
 # Create your views here.
 
 
-def probandoTemplate(request):
-   
-    nom = "Nicolas"
-    ap = "Perez"
-   
-    lista_de_notas = [1,2,3,5,8,3,2]
-    diccionario = {"nombre": nom, "apellido": ap, "hoy": datetime.datetime.now(), "notas": lista_de_notas} # <---- Para enviar al contexto
-   
-    mi_html = open("C:/Users/mteti/Documents/Coderhouse/Python/Proyecto/proyecto_mati/Template/Template.html")
-   
-    plantilla = Template(mi_html.read()) # Se carga en memoria nuestro documento
-    # OJO: Importar Template y Context con: from django.template import Template, Context
-   
-    mi_html.close() # Cerramos el archivo
-   
-    mi_contexto = Context(diccionario) # Le doy al contexto mi nombre y apellido
-    documento = plantilla.render(mi_contexto) # Aca renderizamos la plantilla en documento
-
-    return HttpResponse(documento)
-
 def index(request):
     return render(request, 'index.html')
 
@@ -42,4 +22,5 @@ def curso_create(request):
         curso = Curso(nombre=nombre, numero_comision=numero_comision)
         curso.save()
         return redirect('curso_list')
+     
     return render(request, 'curso_create.html')
